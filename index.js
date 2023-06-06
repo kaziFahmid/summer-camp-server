1ec5379d59650c4ca1dedadd1f9040ed0511f81e
kaziFahmid <nirjhorislam42@gmail.com>
2023-06-06 23:38:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion ,ObjectId} = require('mongodb');
+const app = express()
+const port =process.env.PORT|| 5000
+require('dotenv').config()
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f7zs7lw.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+
+
+
+
+
+
+
+
+
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
