7d8604b54b83410cb503889cc6c5169771b61a1d
kaziFahmid <nirjhorislam42@gmail.com>
2023-06-06 23:43:46 +0600

    id added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port =process.env.PORT|| 5000
 require('dotenv').config()
 app.use(cors())
-
+app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f7zs7lw.mongodb.net/?retryWrites=true&w=majority`;
